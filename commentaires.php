5482ee083f0c53a6986ea1513e44088440aac519
LnLeb <helene.d.m@wanadoo.fr>
2017-08-05 09:36:19 +0200

    Add files via upload

---
+++ commentaires.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Commentaires</title>
+        <link rel="stylesheet" href="css.css">
+    </head>
+    
+    <body>
+        <h1>Mon super blog !</h1>
+        <a href="index.php">Retour à la liste des billets</a>
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <link rel="stylesheet" href="css.css">
+    </head>
+    
+    <body>
+        <h1>Mon super blog !</h1>
+        <p>Derniers billets du blog : </p>
+        
+        
+    </body>
+
+</html>
