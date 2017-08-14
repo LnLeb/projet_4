5ed04d156235f622fa01cc6f767b58e8815a1e76
LnLeb <helene.d.m@wanadoo.fr>
2017-08-14 10:40:17 +0200

    Create index.php

---
+++ vue/blog/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+
+    <head>
+        <meta charset='utf-8'>
+        <title>Mon blog</title>
+        <link rel="stylesheet" href="css.css">
+    </head>
+    <body>
+        <h1>Mon super blog !</h1>
+        <p>Derniers billets du blog :</p>
+        
+        <?php
+        
+        foreach($billets as $billet)
+        {
+        
+        ?>
+        
+        <article id="news">
+            <h3>
+                <?php echo $billet['titre']; ?>
+                <em>Le <?php echo $billet['date_crea']; ?> à <?php echo $billet['heure_crea']; ?></em>            
+            </h3>
+            <p>
+                <?php echo $billet['contenu']; ?><br>
+                <em><a href="commentaires.php?billet=<?php echo $billet['id']; ?>">Commentaires</a></em>
+            </p>
+        </article>
+        <?php
+        }
+        ?>
+    </body>
+
+</html>
