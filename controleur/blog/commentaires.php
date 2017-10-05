51ef5fe69b160be2bf224e95e78f11778afb20e2
LnLeb <helene.d.m@wanadoo.fr>
2017-10-05 15:35:21 +0200

    Update commentaires.php

--- controleur/blog/commentaires.php
+++ controleur/blog/commentaires.php
@@ -1,29 +1,32 @@
 <?php
-
-// on demande le billet correspondant à l'id_billet;
 include_once('../../modele/blog/get_billets.php');
-$billets = get_billets($_GET['billet'], 5);
-
-// on effectue du traitement sur les données (contrôleur)
-// Ici on doit surtout sécurise l'affichage
-foreach($billets as $cle=>$billet)
+if (isset($_GET['billet']))
 {
-    $billets[$cle]['titre'] = htmlspecialchars($billet['titre']);
-    $billets[$cle]['contenu'] = nl2br(htmlspecialchars($billet['contenu']));
+    $billet = get_billet_by_id($_GET['billet']);
+}
+
+if(empty($billet))
+{
+    $billet_vide = true;
 }
 
 // on demande les 5 derniers commentaires qui correspondent au bon billet (modele)
 include_once('../../modele/blog/get_commentaires.php');
-$commentaires = get_commentaires(0, 5, $_GET['billet']);
 
 // on effectue du traitement sur les données (contrôleur)
 // Ici on doit surtout sécurise l'affichage
-
+$commentaires = get_commentaires(0, 10);
+if (!empty($commentaires)) 
+{
 foreach($commentaires as $cle=>$commentaire)
 {
     $commentairess[$cle]['auteur'] = htmlspecialchars($commentaire['auteur']);
     $commentaires[$cle]['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire']));
 }
-
+}
+else
+{
+    $commentaire_vide = true;
+}
 // on affiche la page (vue)
 include_once('../../vue/blog/commentaires.php');
