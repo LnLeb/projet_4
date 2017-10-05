dd00675652e2b69dece2d904474acdddb44defb7
LnLeb <helene.d.m@wanadoo.fr>
2017-10-05 15:34:31 +0200

    Update index.php

--- controleur/blog/index.php
+++ controleur/blog/index.php
@@ -2,7 +2,7 @@
 
 // on demande les 5 derniers billets (modele)
 include_once('../../modele/blog/get_billets.php');
-$billets = get_billets(0,5);
+$billets = get_billets(0, 5);
 
 // on effectue du traitement sur les données (contrôleur)
 // Ici on doit surtout sécurise l'affichage
