d3a3711d4d7eb5797ea9fdcad8c2146af0735afc
LnLeb <helene.d.m@wanadoo.fr>
2017-10-05 15:39:37 +0200

    Update get_commentaires.php

--- modele/blog/get_commentaires.php
+++ modele/blog/get_commentaires.php
@@ -2,19 +2,27 @@
 include_once('../../modele/connexion_sql.php');
 
 // fonction pour récupérer les commentaires
-function get_commentaires($offset, $limit, $id)
+function get_commentaires($offset, $limit)
 {
     global $bdd;
     $offset = (int) $offset;
     $limit = (int) $limit;
-    $id = (int) $id;
 
-    $req = $bdd->prepare('SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y\') AS date_comm, DATE_FORMAT(date_commentaire, \'%Hh%imin%ss\') AS heure_comm FROM commentaires ORDER BY date_commentaire LIMIT :offset, :limit WHERE id_billet = :id');
+    $req = $bdd->prepare('SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y\') AS date_comm, DATE_FORMAT(date_commentaire, \'%Hh%imin%ss\') AS heure_comm FROM commentaires ORDER BY date_commentaire LIMIT :offset, :limit');
     $req->bindParam(':offset', $offset, PDO::PARAM_INT);
     $req->bindParam(':limit', $limit, PDO::PARAM_INT);
-    $req->bindParam(':id', $id);
     $req->execute();
     $commentaires = $req->fetchAll();
 
     return $commentaires;
+}
+if (isset($_POST['id_billet'])) 
+{
+// Intégrer les nouveaux commentaires à la table
+$req = $bdd->prepare('INSERT INTO commentaires (id_billet, auteur, commentaire, date_commentaire) VALUES(:id_billet, :auteur, :commentaire, NOW())');
+$req->execute(array(
+'id_billet'=>$_POST['id_billet'],
+'auteur'=>$_POST['auteur'],
+'commentaire'=>$_POST['comm']
+));
 }
