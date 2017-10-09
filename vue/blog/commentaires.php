fcb5a33e03bae1fc6a468fc53f9c833fba845050
LnLeb <helene.d.m@wanadoo.fr>
2017-10-09 11:13:44 +0200

    Update commentaires.php
    Modification du projet blog du cours en P4

--- vue/blog/commentaires.php
+++ vue/blog/commentaires.php
@@ -1,72 +1,96 @@
+<?php
+if(isset($billet_vide) AND $billet_vide == true)
+{
+    echo 'Erreur numéro billet';
+}
+else
+{
+?>
+
 <!doctype html>
 <html>
 
     <head>
         <meta charset="utf-8">
-        <title>Commentaires</title>
-        <link rel="stylesheet" href="../../vue/blog/css.css">
+        <title><?php echo $billet['titre']; ?></title>
+        <link rel="stylesheet" href="../../vue/blog/projet4.css">
     </head>
     
     <body>
-        <h1>Mon super blog !</h1>
-        <!-- Lien vers la page des billets -->
-        <p><a href="../../controleur/blog/index.php">Retour à la liste des billets</a></p>
-        
-        <?php
-        if(isset($billet_vide) AND $billet_vide == true)
-        {
-            echo 'Erreur numéro billet';
-        }
-        else
-        {
-        ?>
-        <!-- Affichage du bon billet -->
-        <article class="news">
-            <h3>
-                <?php echo $billet['titre']; ?> 
-                <em>Le <?php echo $billet['date_crea']; ?> à <?php echo $billet['heure_crea']; ?></em>
-            </h3>
-            <p><?php echo $billet['contenu']; ?></p>
-        </article>
-
-        <!-- Commentaires -->
-        <h2>Commentaires</h2>
-        
-        <?php 
-        if(isset($commentaire_vide) AND $commentaire_vide == true) {
-            echo 'Pas de commentaires';
-        }
-        else
-        {
-        foreach($commentaires as $commentaire)
-        {
-            if ($commentaire['id_billet'] == $_GET['billet'])
-            {
-        ?>
-        <p>
-            <strong><?php echo $commentaire['auteur']; ?></strong> Le <?php echo $commentaire['date_comm']; ?> à <?php echo $commentaire['heure_comm']; ?><br>
-            <?php echo $commentaire['commentaire']; ?>
-        </p>
-        
-        <?php
-            }
-        }
-        }
-        ?>
-        
-        <!-- Ajout de commentaires -->
-        <form method="post" action="../../modele/blog/get_commentaires.php">
-            <p>
-                <label for="auteur">Pseudo : </label><br>
-                <input type="text" name="auteur" id="auteur"><br>
-                <label for="comm">Commentaire :</label><br>
-                <textarea name="comm" id="comm" rows="4" cols="25"></textarea><br>
-                <input type="hidden" name="id_billet" value="<?php echo $_GET['billet']; ?>">
-                <input type="submit" value="Valider">
-            </p>
-        </form>
-        <?php
-        }
-        ?>
+         <header>
+            <h1><a href="../../controleur/blog/index.php">Billet simple pour l'Alaska</a></h1>
+            <p>Jean Forteroche</p>
+        </header>
+        <section>
+            <aside>
+                <p>Connexion à l'administration : </p>
+                <form method="post" action="../../modele/blog/admin.php">
+                    <label for="identifiant">Identifiant : </label>
+                    <input type="text" name="identifiant" id="identifiant"> <br>
+                    <label for="motdepasse">Mot de passe : </label>
+                    <input type="password" name="motdepasse" id="motdepasse"> <br>
+                    <input type="submit" value="Valider">
+                </form>
+            </aside>
+            <article>
+                <h2>Chapitre <?php echo $billet['id']; ?></h2>
+                <h3>
+                    <?php echo $billet['titre']; ?>
+                </h3>
+                <p>
+                    <em>Posté le <?php echo $billet['date_crea']; ?> à <?php echo $billet['heure_crea']; ?></em>
+                </p>
+                <p>
+                    <?php echo $billet['contenu']; ?>
+                </p>
+            </article>
+            <article>
+                <h2>Commentaires</h2>
+                
+                <?php 
+                if (isset($commentaire_vide) AND $commentaire_vide == true)
+                {
+                    echo 'Pas de commentaires';
+                }
+                else 
+                {
+                    foreach($commentaires as $commentaire)
+                    {
+                        if ($commentaire['id_billet'] == $_GET['billet'])
+                        {
+                ?>
+                  
+                <h3>
+                    <?php echo $commentaire['auteur']; ?>
+                </h3>
+                <p>
+                    <em>Posté le <?php echo $commentaire['date_comm']; ?> à <?php echo $commentaire['heure_comm']; ?></em>
+                </p>
+                <p>
+                    <?php echo $commentaire['commentaire']; ?>
+                </p>
+                
+                <?php
+                        }
+                    }
+                }
+                ?>
+                
+                <form method="post" action="../../modele/blog/add_commentaires.php">
+                    <p>
+                        <label for="auteur">Pseudo : </label><br>
+                        <input type="text" name="auteur" id="auteur"><br>
+                        <label for="comm">Commentaire : </label><br>
+                        <textarea name="comm" id="comm" rows="4" cols="25"></textarea><br>
+                        <input type="hidden" name="id_billet" value="<?php echo $GET_['billet']; ?>">
+                        <input type="submit" value="Valider">
+                    </p>
+                </form>                
+            </article>
+        </section>
     </body>
 </html>
+
+<?php
+}
+?>
