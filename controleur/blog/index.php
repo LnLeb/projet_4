55f08c58439d2a445172f4ff098368ddcef4cfb6
LnLeb <helene.d.m@wanadoo.fr>
2017-10-09 11:04:06 +0200

    Update index.php
    modification du projet blog du cours en P4

--- controleur/blog/index.php
+++ controleur/blog/index.php
@@ -1,16 +1,15 @@
 <?php
 
-// on demande les 5 derniers billets (modele)
-include_once('../../modele/blog/get_billets.php');
+// on demande les 5 derniers billets au modele
+include_once('../../modele/blog/get_billet.php');
 $billets = get_billets(0, 5);
 
-// on effectue du traitement sur les données (contrôleur)
-// Ici on doit surtout sécurise l'affichage
+// on sécurise l'affichage
 foreach($billets as $cle=>$billet)
 {
     $billets[$cle]['titre'] = htmlspecialchars($billet['titre']);
     $billets[$cle]['contenu'] = nl2br(htmlspecialchars($billet['contenu']));
 }
 
-// on affiche la page (vue)
+// on affiche la page via l'index dans la vue
 include_once('../../vue/blog/index.php');
