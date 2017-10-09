5f450bacddaa36b392be0ac571a1df06ded497fe
LnLeb <helene.d.m@wanadoo.fr>
2017-10-09 11:07:21 +0200

    Update connexion_sql.php
    modification du projet blog du cours en P4

--- modele/connexion_sql.php
+++ modele/connexion_sql.php
@@ -1,10 +1,10 @@
- <?php 
-        // récupération de la base de données
-        try
-        {
-            $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-        }
-        catch (Exception $e)
-        {
-            die('Erreur : ' .$e->getMessage());
-        }
+<?php
+    // récupération de la base de données
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : problème de connexion à la base de données : ' .$e->getMessage());
+    }
