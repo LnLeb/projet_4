a29a15b6acc2c9ab517b6040c6eebfec7dad013b
LnLeb <helene.d.m@wanadoo.fr>
2017-10-12 17:28:23 +0200

    Add files via upload
    Architecture MVC avec un contrôleur frontal : réorganisation des fichiers

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require('controleur/controleur.php');
+
+try 
+{
+    if (isset($_GET['action'])) 
+    {
+        if ($_GET['action'] == 'billet') {
+            if (isset($_GET['id'])) {
+                $idBillet = intval($_GET['id']);
+                if ($idBillet != 0) {
+                    billet($idBillet);
+                } 
+                else {
+                    throw new Exception("Identifiant de billet non valide");
+                }
+            } 
+            else {
+                throw new Exception("Identifiant de billet non défini");
+            }
+        } 
+        else if ($_GET['action'] == 'admin') {
+            admin();
+        }
+        else {
+            throw new Exception("Action non valide");
+        }
+    }
+    else {
+        accueil();  // action par défaut
+    }
+}
+catch (Exception $e) {
+    erreur($e->getMessage());
+}
