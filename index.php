100b95c8f204c1d2dc10eff0de3cc178bd58d2c9
LnLeb <helene.d.m@wanadoo.fr>
2017-10-22 12:32:38 +0200

    Update index.php
    Adaptation de l'index au passage du contrôleur à la POO

--- index.php
+++ index.php
@@ -1,36 +1,6 @@
 <?php
 
-require('controleur/controleur.php');
+require('controleur/Routeur.php');
 
-try 
-{
-    if (isset($_GET['action'])) 
-    {
-        if ($_GET['action'] == 'billet') {
-            if (isset($_GET['id'])) {
-                $idBillet = intval($_GET['id']);
-                if ($idBillet != 0) {
-                    billet($idBillet);
-                } 
-                else {
-                    throw new Exception("Identifiant de billet non valide");
-                }
-            } 
-            else {
-                throw new Exception("Identifiant de billet non défini");
-            }
-        } 
-        else if ($_GET['action'] == 'admin') {
-            admin();
-        }
-        else {
-            throw new Exception("Action non valide");
-        }
-    }
-    else {
-        accueil();  // action par défaut
-    }
-}
-catch (Exception $e) {
-    erreur($e->getMessage());
-}
+$routeur = new Routeur();
+$routeur->routerRequete();
