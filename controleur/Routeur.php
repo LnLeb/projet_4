70c72f2514682675cb730e7929484427438d229b
LnLeb <helene.d.m@wanadoo.fr>
2017-10-23 14:24:10 +0200

    Update Routeur.php
    Ajout de commentaires pour repérer les erreurs et clarifier le code.  + ajout des paramètres pour la pagination des commentaires

--- controleur/Routeur.php
+++ controleur/Routeur.php
@@ -1,7 +1,6 @@
 <?php
 
-// autoload de chargement des classes
-
+// autoload de chargement des classes depuis le dossier contrôleur
 function chargerClasse($classe)
 {
   require 'controleur/'. $classe . '.php';
@@ -9,6 +8,8 @@ function chargerClasse($classe)
 
 spl_autoload_register('chargerClasse');
 
+
+// définition de la classe Routeur qui gère l'affichage des pages en fonction des paramètres transmis par l'url ou affiche des messages d'erreur
 class Routeur
 {
     private $ctrlAccueil,
@@ -28,30 +29,58 @@ class Routeur
     {
         try 
         {
-            if (isset($_GET['action']))  {
-                if ($_GET['action'] == 'billet') {
-                    if (isset($_GET['id'])) {
+            // si on tente de définir une action par l'url...
+            if (isset($_GET['action']))  
+            {
+                // si l'action définie est billet : on aura à afficher un chapitre uniquement en fonction de son id
+                if ($_GET['action'] == 'billet') 
+                {
+                    if (isset($_GET['id'])) 
+                    {
                         $idBillet = intval($_GET['id']);
-                        if ($idBillet != 0) {
+                        if ($idBillet != 0) 
+                        {
                             $this->ctrlBillet->billet($idBillet);
+                            // Si l'action et l'id sont bien définis, on définit la pagination pour les commentaires 
+                            if(isset($_GET['page']))
+                            {
+                                $page = intval($_GET['page']);
+                                if ($page != 0)
+                                {
+                                    $this->ctrlCommentaire->commentaire($page);
+                                }
+                            }
+                            // par défaut, la page des commentaires affichée sera la page 1
+                            else
+                            {
+                                $page = intval($_GET['page']);
+                                $page = 1;
+                            }
                         } 
-                        else {
+                        else 
+                        {
                             throw new Exception("Identifiant de billet non valide");
                         }
                     } 
-                    else {
+                    else 
+                    {
                         throw new Exception("Identifiant de billet non défini");
                     }
                 } 
+                // si l'action définie est admin : on sera sur la page d'administration du site 
                 else if ($_GET['action'] == 'admin') {
                     $this->ctrlAdmin->admin();
                 }
-                else {
+                // si l'action définie n'est pas valable
+                else 
+                {
                     throw new Exception("Action non valide");
                 }
             }
-            else {
-                $this->ctrlAccueil->accueil();  // action par défaut
+            // si l'action n'est pas définie : par défaut on sera sur la page d'accueil
+            else 
+            {
+                $this->ctrlAccueil->accueil();
             }
         }
         catch (Exception $e) {
