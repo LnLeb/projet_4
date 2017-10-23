b2a3d730cc8ed47896592d7847831d97da44145b
LnLeb <helene.d.m@wanadoo.fr>
2017-10-23 11:14:23 +0200

    Update ControleurAdmin.php
    modifications POO

--- controleur/ControleurAdmin.php
+++ controleur/ControleurAdmin.php
@@ -1,18 +1,40 @@
 <?php
 
+require_once ('modele/BilletManager.php');
+require_once ('modele/CommentaireManager.php');
+require_once ('vue/Vue.php');
+
 class ControleurAdmin
 {
+    
+        private $billet;
+    
+    public function __construct()
+    {
+        $this->billet = new BilletManager();
+    }
+    
     public function admin()
     {
         if ((isset($_POST['identifiant']) AND $_POST['identifiant'] == "Jean.Forteroche" AND isset($_POST['motdepasse']) AND $_POST['motdepasse'] == "alaska2.0") OR (isset($_SESSION['identifiant'])))
         {
             $_SESSION['identifiant']=$_POST['identifiant'];
+            
+            $billets = $this->billet->get_billets(0, 500);
+        
+            // sécurisation de l'affichage
+            foreach($billets as $cle=>$this->billet)
+            {
+                $billets[$cle]['titre'] = htmlspecialchars($this->billet->titre());
+            }
+            
             // on affiche la page (vue)
-            require('vue/admin.php');
+            $vue = new Vue('admin');
+            $vue->generer(array('billet' => $billet));
         }
         else 
         {
-            require('vue/accueil.php');
+            require('index.php');
         }
     }
     
@@ -21,6 +43,6 @@ class ControleurAdmin
         if(isset($_SESSION['identifiant'])) {
         session_destroy(); 
         } 
-        require('vue/accueil.php');
+        require('index.php');
     }
 }
