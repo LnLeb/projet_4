c19be8ce5bfb4b7976936cd4fde3f85324cb1c7c
LnLeb <helene.d.m@wanadoo.fr>
2017-10-23 11:17:02 +0200

    Update accueil.php
    Passage à la POO pour la vue

--- vue/accueil.php
+++ vue/accueil.php
@@ -1,40 +1,30 @@
-<?php $title = 'Billet simple pour l\'Alaska'; ?>
+<?php $this->titre = 'Billet simple pour l\'Alaska'; ?>
 
-<?php ob_start(); ?>
-    <h2>Présentation du concept : </h2>
-    <p>"Amis lecteurs, je vous souhaite la bienvenue dans cette toute nouvelle aventure! J'ai décidé d'écrire ce troisième roman de façon toute particulière : je posterai les chapitres sur ce blog, les uns après les autres, une fois qu'ils seront rédigés, et m'aiderai de vos commentaires pour écrire la suite. Etes-vous prêts à partager cette histoire avec moi? Inspirée par vous, écrite pour vous, en voici le commencement..." <br>
-    Jean Forteroche</p>
-    <h2>Derniers chapitres :</h2>
+<img src="contenu/img/alaska.jpg" alt="Alaska">
+<p>"Amis lecteurs, je vous souhaite la bienvenue dans cette toute nouvelle aventure! J'ai décidé d'écrire ce troisième roman de façon toute particulière : je posterai les chapitres sur ce blog, les uns après les autres, une fois qu'ils seront rédigés, et m'aiderai de vos commentaires pour écrire la suite. Etes-vous prêts à partager cette histoire avec moi? Inspirée par vous, écrite pour vous, en voici le commencement..." <br>
+Jean Forteroche</p>
 
-    <?php
-    foreach($billets as $billet)
-        {
-    ?>
+<h2>Derniers chapitres publiés :</h2>
+
+<?php
+$billets = new ControleurAccueil;
+foreach($billets as $billet)
+{
+?>
     <h3><?php echo $billet['titre']; ?></h3>
     <p>
         <em>Posté le <?php echo $billet['date_crea']; ?> à <?php echo $billet['heure_crea']; ?></em>
     </p>
     <p>
         <?php echo $billet['extrait']; ?><br>
-        <em><a href="index.php?action=billet&id=<?php echo $billet['id']; ?>">Lire tout le chapitre</a></em>
+        <em><a href="index.php?action=billet&id=<?php echo $billet['id']; ?>">Lire le chapitre</a></em>
     </p>
-    <?php
-    }
-    include_once('modele/modele.php');
-    echo 'Page : ';
-    for($i=1; $i<$nb_pages+1; $i++)
-    {
-        ?>
-        <a href="index.php?page=<?php echo $i ?>"> <?php echo $i ?> | </a>
-    <?php
-    }
-    ?>
-<?php $article = ob_get_clean(); ?>
-
-<?php ob_start(); ?>
-    <h3>Présentation de l'auteur : </h3>
-    <img src="contenu/img/portrait.jpg" alt="portrait Jean Forteroche">
-    <p>Jean Forteroche est né en 1955 à Paris. Après de brillantes études à la Sorbonne, il décide de tout quitter et part seul en Alaska. De retour en France, il entreprend d'écrire des romans pour raconter son aventure. "Billet simple pour l'Alaska" fait suite à "Seul au bout du monde" et "La neige et le froid". </p>
-<?php $aside = ob_get_clean(); ?>
-
-<?php require 'gabarit.php'; ?>
+<?php
+}
+?>
+<aside>
+<img src="contenu/img/portrait.jpg" alt="portrait Jean Forteroche">
+<h2>Présentation de l'auteur : </h2>
+<p>Jean Forteroche est né en 1955 à Paris. Après de brillantes études à la Sorbonne, il décide de tout quitter et part seul en Alaska. De retour en France, il entreprend d'écrire des romans pour raconter son aventure. "Billet simple pour l'Alaska" fait suite à "Seul au bout du monde" et "La neige et le froid". </p>
+<h2>Commentaires récents : </h2>
+</aside>
