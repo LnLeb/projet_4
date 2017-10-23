2b0fbd998b8684cfdb5b16499a53e6de46f1363e
LnLeb <helene.d.m@wanadoo.fr>
2017-10-23 11:17:36 +0200

    Update admin.php
    Passage à la POO pour la vue

--- vue/admin.php
+++ vue/admin.php
@@ -1,9 +1,26 @@
-<?php $title = 'Administration'; ?>
+<?php $this->titre = 'Administration'; ?>
 
-<?php ob_start(); ?>
-        <div id="admin">
-            <h2>Interface de gestion du blog</h2>
-            <h3>Création d'un nouveau billet</h3>
+<div id="admin">
+    <h2>Interface de gestion du blog</h2>
+            
+    <h3>Chapitres en ligne : </h3>
+    <table>
+        <?php
+            foreach($billets as $billet)
+            {
+            ?>
+                <tr>
+                    <h4><?php echo $billet['titre']; ?></h4>
+                    <a href="index.php?action=billet&id=<?php echo $billet['id']; ?>">Lire</a> |
+                    <a href="">Mettre à jour</a> |
+                    <a href="">Supprimer</a> 
+                </tr>
+            <?php
+            }
+            ?>
+            </table> 
+            
+            <h3>Création d'un nouveau billet : </h3>
             <form method="post" action="index.php?action=admin">
                 <label for="titre">Titre : </label><br>
                 <input type="text" name="titre" id="titre"><br>
@@ -13,9 +30,11 @@
                 <textarea name="contenu" id="contenu" rows="40" cols="150"></textarea><br>
                 <input type="submit" value="publier">
             </form>
+            
+            <h3>Nouveaux commentaires : </h3>
+            <table>
+            
+            
+            
+            </table>
         </div>
-<?php $article = ob_get_clean(); ?>
-
-<?php $aside = ''; ?>
-
-<?php require 'gabarit.php'; ?>
