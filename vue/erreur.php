f9fc096347e83b35925a4f56cb7737c9310acd24
LnLeb <helene.d.m@wanadoo.fr>
2017-10-23 11:19:01 +0200

    Update erreur.php
    Passage à la POO pour la vue

--- vue/erreur.php
+++ vue/erreur.php
@@ -1,9 +1,3 @@
-<?php $title = 'Billet simple pour l\'Alaska'; ?>
+<?php $this->titre = 'Billet simple pour l\'Alaska'; ?>
 
-<?php ob_start() ?>
 <p>Une erreur est survenue : <?= $msgErreur ?></p>
-<?php $article = ob_get_clean(); 
-
-$aside = '';
-
-require 'vue/gabarit.php'; ?>
