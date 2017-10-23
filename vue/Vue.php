f5e0c604ff0a7117c203d3fab002d1b94345823c
LnLeb <helene.d.m@wanadoo.fr>
2017-10-23 11:16:22 +0200

    Add files via upload
    Passage à la POO pour la vue

---
+++ vue/Vue.php
@@ -0,0 +1,36 @@
+<?php
+
+class Vue
+{
+    private $fichier;
+    private $titre;
+    
+    public function __construct($action)
+    {
+        $this->fichier = 'vue/' . $action . '.php';
+    }
+    
+    public function generer($donnees)
+    {
+        $contenu = $this->genererFichier($this->fichier, $donnees);
+        $vue = $this->genererFichier('vue/gabarit.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        
+        echo $vue;
+    }
+    
+    private function genererFichier($fichier, $donnees)
+    {
+        if (file_exists($fichier))
+        {
+            extract($donnees);
+            ob_start();
+            require $fichier;
+            
+            return ob_get_clean();
+        }
+        else
+        {
+            throw new Exception("Fichier '$fichier' introuvable");
+        }
+    }
+}
