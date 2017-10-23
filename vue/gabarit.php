6db4e355dcbf97e79d38d80395fd35b4b1b49191
LnLeb <helene.d.m@wanadoo.fr>
2017-10-23 11:19:42 +0200

    Update gabarit.php
    Passage à la POO pour la vue et modification du gabarit

--- vue/gabarit.php
+++ vue/gabarit.php
@@ -5,7 +5,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title><?= $title ?></title>
+        <title><?php $title ?></title>
         <link rel="stylesheet" href="contenu/projet4.css?<? echo time(); ?>">
         <link rel="icon" href="contenu/img/favicon.jpg">
     </head>
@@ -13,31 +13,38 @@
     <body>
         <div id="conteneur">
             <header>
-                <h1><a href="index.php">Billet simple pour l'Alaska</a></h1>
+                <h1>Billet simple pour l'Alaska</h1>
                 <p>Jean Forteroche</p>
+                <nav>
+                    <ul>
+                        <li>Accueil</li>
+                        <li>Les chapitres</li>
+                        <li>Connexion</li>
+                    </ul>
+                </nav>
+                <ul id="chapitres">
+                <?php foreach($billets as $this->billet->accueil()->$allBillets);
+                {?>
+                    <li><a href="index.php?action=billet&id=<?php echo $billet['id']; ?>"><?php echo $billet['titre']; ?></a></li>
+                <?php }?>
+                </ul>
+                <form method="post" action="index.php?action=admin" id="connexion">
+                    <label for="identifiant">Identifiant : </label>
+                    <input type="text" name="identifiant" id="identifiant"><br>
+                    <label for="motdepasse">Mot de passe : </label>
+                    <input type="password" name="motdepasse" id="motdepasse"><br>
+                    <input type="submit" value="Connexion">
+                    <button><a href="index.php">Déconnexion</a></button>
+                </form>
+                <?php 
+                if (isset($_POST['identifiant']) AND $_POST['identifiant'] != "Jean.Forteroche" AND isset($_POST['motdepasse']) AND $_POST['motdepasse'] != "alaska2.0") 
+                {
+                    echo '<p id="erreur">Identifiant ou mot de passe incorrect</p>';
+                }
+                ?>
             </header>
             <section>
-                <article>
-                    <?= $article ?>
-                </article>
-                <aside>
-                    <?= $aside ?>
-                    <h3>Connexion à l'administration</h3>
-                    <form method="post" action="index.php?action=admin">
-                        <label for="identifiant">Identifiant : </label>
-                        <input type="text" name="identifiant" id="identifiant"><br>
-                        <label for="motdepasse">Mot de passe : </label>
-                        <input type="password" name="motdepasse" id="motdepasse"><br>
-                        <input type="submit" value="Connexion">
-                        <button><a href="index.php">Déconnexion</a></button>
-                    </form>
-                    <?php 
-                    if (isset($_POST['identifiant']) AND $_POST['identifiant'] != "Jean.Forteroche" AND isset($_POST['motdepasse']) AND $_POST['motdepasse'] != "alaska2.0") 
-                    {
-                    echo '<p id="erreur">Identifiant ou mot de passe incorrect</p>';
-                    }
-                    ?>
-                </aside>
+                <?php $contenu ?>
             </section>
             <footer>
                 <p>Site école réalisé par Hélène Leblanc pour OpenClassrooms, projet 4 du parcours Développeur Web Junior<br>
