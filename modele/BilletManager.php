e2e741a7df125a7b000a19ac4eb64f15918cc1b1
LnLeb <helene.d.m@wanadoo.fr>
2017-10-24 19:29:41 +0200

    Update BilletManager.php
    Correction des requêtes sql

--- modele/BilletManager.php
+++ modele/BilletManager.php
@@ -10,26 +10,34 @@ class BilletManager extends Modele
 {
     // méthode pour récupérer tous les billets
     public function get_billets($offset, $limit)
-    {
+    {       
         $offset = (int)$offset;
-        $limit = (int)$limit;  
+        $limit = (int)$limit;
         
         $sql = 'SELECT id, titre, extrait, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS date_crea, DATE_FORMAT(date_creation, \'%Hh%i\') AS heure_crea FROM billets ORDER BY id LIMIT :offset, :limit';
         
-        $billets = $this->executerRequete($sql, array($offset, $limit));
-    
-        return $billets->fetchAll();
+        $req = $this->executerRequete($sql, array('offset' => $offset, 'limit' => $limit));
+        $req->bindParam(':offset', $offset, PDO::PARAM_INT);
+        $req->bindParam(':limit', $limit, PDO::PARAM_INT);
+        $req->execute();
+        
+        $billets = $req->fetchAll();
+        return $billets;
     }
     
     // méthode pour récupérer un seul billet en fonction de son id
     public function getBilletById($id_billet)
     {
+        $id_billet = (int)$id_billet;
+        
         $sql = 'SELECT id, titre, extrait, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS date_crea, DATE_FORMAT(date_creation, \'%Hh%i\') As heure_crea FROM billets WHERE id=:id_billet';
-        $billet = $this->executerRequete($sql, array($id_billet));
+        
+        $req = $this->executerRequete($sql, array($id_billet));
+        $req->bindParam(':id_billet', $id_billet, PDO::PARAM_INT);
+        $req->execute();
 
-        $sql->bindParam(':id_billet', $id_billet, PDO::PARAM_INT);
-
-        return $billet->fetch();
+        $billet = $req->fetch();
+        return $billet;
     }
     
     // méthode pour poster un nouveau billet dans la BDD
@@ -39,15 +47,16 @@ class BilletManager extends Modele
         {
             $sql = 'INSERT INTO billets (titre, extrait, contenu, date_creation) VALUES(:titre, :extrait, :contenu, NOW())';
             
-            $sql->bindValue(':titre', $billet->titre());
-            $sql->bindValue(':extrait', $billet->extrait());
-            $sql->bindValue(':contenu', $billet->contenu());
-            
-            $this->executerRequete($sql, array('titre'=>$_POST['titre'],
+            $req = $this->executerRequete($sql, array('titre'=>$_POST['titre'],
             'extrait'=>$_POST['extrait'],
             'contenu'=>$_POST['contenu']));
+            $req->bindValue(':titre', $billet->titre());
+            $req->bindValue(':extrait', $billet->extrait());
+            $req->bindValue(':contenu', $billet->contenu());
             
-            $billet->hydrate([
+            $req->execute();
+            
+            $req->hydrate([
                 'id' => $this->bdd->lastInsertId()
             ]);
         }
@@ -56,27 +65,33 @@ class BilletManager extends Modele
     // méthode pour supprimer un billet de la BDD
     public function delete_billet(Billet $billet)
     {
-        $sql = 'DELETE FROM billets WHERE id ='.$billet->id();
-        $this->executerRequete($sql);
+        $sql = 'DELETE FROM billets WHERE id = :id';
+        $req = $this->executerRequete($sql, array('id' => $billet->id()));
+        $req->bindParam(':id', $billet->id(), PDO::PARAM_INT);
+        
+        $req->execute();
     }
     
     // méthode pour modifier un billet de la BDD
     public function update_billet(Billet $billet)
     {
         $sql = 'UPDATE billets SET titre = :titre, contenu = :contenu, extrait = :extrait WHERE id = :id';
-        $sql->bindValue(':titre', $billet->titre(), PDO::PARAM_INT);
-        $sql->bindValue(':contenu', $billet->contenu(), PDO::PARAM_INT);
-        $sql->bindValue(':extrait', $billet->extrait(), PDO::PARAM_INT);
-        $this->executerRequete($sql);
+        $req = $this->executerRequete($sql, array('titre' => $_POST['titre'], 'extrait' => $_POST['extrait'], 'contenu' => $_POST['contenu']));
+        $req->bindValue(':titre', $billet->titre(), PDO::PARAM_INT);
+        $req->bindValue(':contenu', $billet->contenu(), PDO::PARAM_INT);
+        $req->bindValue(':extrait', $billet->extrait(), PDO::PARAM_INT);
+        
+        $req->execute();
     }
     
     // méthode pour renvoyer le nombre total de billets
     public function count_billets()
     {
         $sql = 'SELECT COUNT(*) AS nb_billets FROM billets';
+        $req = $this->executerRequete($sql);
+        $resultat = $req->fetch();
+        $nb_billets = (int)$resultat['nb_billets'];
         
-        $donnees = $this->executerRequete($sql);
-        $nb_billets = $donnees['nb_billets'];
         return $nb_billets;
     }
 }
