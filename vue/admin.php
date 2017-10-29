726babeb400ce9da07284fdfabb6d0beb0ab5249
LnLeb <helene.d.m@wanadoo.fr>
2017-10-29 18:10:19 +0100

    Update admin.php
    mise en page

--- vue/admin.php
+++ vue/admin.php
@@ -1,40 +1,50 @@
 <?php $this->titre = 'Administration'; ?>
+<nav>
+    <ul>
+        <li><a href='#chapitresEnLigne'>Chapitres en ligne</a></li>
+        <li><a href='#nouveauChapitre'>Nouveau chapitre</a></li>
+        <li><a href='#gestionComm'>Gestion des commentaires</a></li>
+        <li><a href='index.php'>Déconnexion</a></li>
+    </ul>
+</nav>
 
-<div id="admin">
-    <h2>Interface de gestion du blog</h2>
-            
-    <h3>Chapitres en ligne : </h3>
-    <table>
-        <?php
+<section id="admin">
+    <article id="chapitresEnLigne">
+        <h2>Chapitres en ligne : </h2>
+        <table>
+            <?php
             foreach($billets as $billet)
             {
             ?>
-                <tr>
-                    <h4><?php echo $billet['titre']; ?></h4>
-                    <a href="index.php?action=billet&id=<?php echo $billet['id']; ?>">Lire</a> |
-                    <a href="">Mettre à jour</a> |
-                    <a href="">Supprimer</a> 
-                </tr>
+            <tr>
+                <h3><?php echo $billet['titre']; ?></h3>
+                <a href="index.php?action=billet&id=<?php echo $billet['id']; ?>&page=1">Lire</a> |
+                <a href="">Mettre à jour</a> |
+                <a href="">Supprimer</a> 
+            </tr>
             <?php
             }
             ?>
-            </table> 
+        </table> 
+    </article>
+    <article id="nouveauChapitre">
+        <h2>Création d'un nouveau billet : </h2>
+        <form method="post" action="index.php?action=admin">
+            <label for="titre">Titre : </label><br>
+            <input type="text" name="titre" id="titre"><br>
+            <label for="extrait">Extrait : </label><br>
+            <textarea name="extrait" id="extrait" rows="10" cols="150"></textarea><br>
+            <label for="contenu">Contenu : </label><br>
+            <textarea name="contenu" id="contenuAdmin" rows="40" cols="150"></textarea><br>
+            <input type="submit" value="publier">
+        </form>
+    </article>
+    <article id="gestionComm">
+        <h2>Nouveaux commentaires : </h2>
+        <table>
             
-            <h3>Création d'un nouveau billet : </h3>
-            <form method="post" action="index.php?action=admin">
-                <label for="titre">Titre : </label><br>
-                <input type="text" name="titre" id="titre"><br>
-                <label for="extrait">Extrait : </label><br>
-                <textarea name="extrait" id="extrait" rows="10" cols="150"></textarea><br>
-                <label for="contenu">Contenu : </label><br>
-                <textarea name="contenu" id="contenu" rows="40" cols="150"></textarea><br>
-                <input type="submit" value="publier">
-            </form>
-            
-            <h3>Nouveaux commentaires : </h3>
-            <table>
             
             
-            
-            </table>
-        </div>
+        </table>
+    </article>
+</section>
