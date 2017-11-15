ec98e182d042ee32ea70b67f743be2ca9a042d52
LnLeb <helene.d.m@wanadoo.fr>
2017-11-15 14:36:35 +0100

    Update accueil.php
    adaptation aux modifications, remise en page, indentation, commentaires, etc...

--- vue/accueil.php
+++ vue/accueil.php
@@ -1,21 +1,20 @@
 <?php 
-// titre de la page
 $this->titre = 'Billet simple pour l\'Alaska'; 
 ?>
-
-<nav>
-    <ul>
-        <li><a href="index.php">Accueil</a></li>
-        <li>Les chapitres</li>
-        <li>Connexion</li>
-    </ul>
-</nav>
+<article id="navigation">
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="#chapitres">Les chapitres</a></li>
+            <li><a href="#connexion">Connexion</a></li>
+        </ul>
+    </nav>
     <!-- Affichage des chapitres dans la navigation -->
     <ul id="chapitres">
     <?php foreach($allBillets as $billet)
     {
     ?>
-        <li><a href="index.php?action=billet&id=<?php echo $billet['id']; ?>&page=1"><?php echo $billet['titre']; ?></a></li>
+        <li><a href="index.php?action=billet&id=<?= $billet['id']; ?>&page=1"><?= $billet['titre']; ?></a></li>
         <?php }?>
     </ul>
     <!-- Affichage de la connexion dans la navigation -->
@@ -25,14 +24,9 @@ $this->titre = 'Billet simple pour l\'Alaska';
         <label for="motdepasse">Mot de passe : </label>
         <input type="password" name="motdepasse" id="motdepasse"><br>
         <input type="submit" value="Connexion">
-        <button><a href="index.php">Déconnexion</a></button>
-        <?php
-        if (isset($_POST['identifiant']) AND $_POST['identifiant'] != "Jean.Forteroche" AND isset($_POST['motdepasse']) AND $_POST['motdepasse'] != "alaska2.0") 
-        {
-            echo '<p id="erreur">Identifiant ou mot de passe incorrect</p>';
-        }
-        ?>
+        <a href="index.php?action=admin&rubrique=deconnexion"><button>Déconnexion</button></a>
     </form>
+</article>
 
 <section id="contenuAccueil">
     <article id="presentation">
@@ -50,14 +44,14 @@ $this->titre = 'Billet simple pour l\'Alaska';
         {
         ?>
             <div>
-                <h3><?php echo $billet['titre']; ?></h3>
+                <h3><?= $billet['titre']; ?></h3>
                 <p>
-                    <em>Posté le <?php echo $billet['date_crea']; ?> à <?php echo $billet['heure_crea']; ?></em>
+                    <em>Posté le <?= $billet['dateCrea']; ?> à <?= $billet['heureCrea']; ?></em>
                 </p>
                 <p>
-                    <?php echo $billet['extrait']; ?>
+                    <?= $billet['extrait']; ?>
                 </p>
-                <em><a href="index.php?action=billet&id=<?php echo $billet['id']; ?>&page=1">Lire le chapitre</a></em>
+                <em><a href="index.php?action=billet&id=<?= $billet['id']; ?>&page=1">Lire le chapitre</a></em>
             </div>
         <?php
         }
@@ -65,23 +59,24 @@ $this->titre = 'Billet simple pour l\'Alaska';
         </div>
     </article>
 
+    <!-- présentation de l'auteur et derniers chapitres publiés -->
     <aside>
         <img src="contenu/img/portrait.jpg" alt="portrait Jean Forteroche">
         <h2>Présentation de l'auteur : </h2>
         <p>Jean Forteroche est né en 1955 à Paris. Après de brillantes études à la Sorbonne, il décide de tout quitter et part seul en Alaska. De retour en France, il entreprend d'écrire des romans pour raconter son aventure. "Billet simple pour l'Alaska" fait suite à "Seul au bout du monde" et "La neige et le froid". </p>
         
         <!-- trois derniers commentaires publiés -->
-        <h2>Commentaires récents : </h2>
+        <h2 id="comRec">Commentaires récents : </h2>
         <?php 
         foreach($derniersCommentaires as $commentaire)
         {
-        ?>
-            <h3><?php echo $commentaire['auteur']; ?></h3>
-            <p>
-                <em>Posté le <?php echo $commentaire['date_comm']; ?> à <?php echo $commentaire['heure_comm']; ?></em>
-            </p>
-            <p><?php echo $commentaire['commentaire']; ?></p>
-        <?php
+            ?>
+                <h3><?= $commentaire['auteur']; ?></h3>
+                <p>
+                    <em>Posté le <?= $commentaire['dateComm']; ?> à <?= $commentaire['heureComm']; ?></em>
+                </p>
+                <p><?= $commentaire['commentaire']; ?></p>
+            <?php
         }
         ?>
     </aside>
