9062659f97ec64105672d8247cd1ca498e182edf
LnLeb <helene.d.m@wanadoo.fr>
2017-11-15 14:34:05 +0100

    Update CommentaireManager.php
    modification des requêtes préparées, gestion d'un nouvel argument 'valide' pour permettre de signaler les commentaires, remise en page

--- modele/CommentaireManager.php
+++ modele/CommentaireManager.php
@@ -8,54 +8,92 @@ require_once('modele/Commentaire.php');
 // Création de la classe CommentaireManager qui effectuera les requêtes en lien avec les commentaires
 class CommentaireManager extends Modele
 {
-    // Récupération des commentaires
-    public function get_commentaires($offset, $limit)
+    // Récupération des commentaires signalés à valider
+    public function getCommentairesAValider($offset, $limit)
     {
-        $sql = 'SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y\') AS date_comm, DATE_FORMAT(date_commentaire, \'%Hh%i\') AS heure_comm FROM commentaires ORDER BY date_commentaire LIMIT ?, ?';
+        $offset = (int)$offset;
+        $limit = (int)$limit;
+        $sql = 'SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y\') AS dateComm, DATE_FORMAT(date_commentaire, \'%Hh%i\') AS heureComm, valide FROM commentaires WHERE valide=\'FALSE\' ORDER BY date_commentaire LIMIT :offset, :limit';
+        
+        $req = $this->executerRequete($sql, array($offset, $limit));
+        $commentaires = $req->fetchAll();
+        return $commentaires;
+    }
+    // Récupération des commentaires validés
+    public function getCommentairesValide($offset, $limit)
+    {
+        $offset = (int)$offset;
+        $limit = (int)$limit;
+        $sql = 'SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y\') AS dateComm, DATE_FORMAT(date_commentaire, \'%Hh%i\') AS heureComm, valide FROM commentaires WHERE valide=\'TRUE\' ORDER BY date_commentaire LIMIT :offset, :limit';
+        
         $req = $this->executerRequete($sql, array($offset, $limit));
         $commentaires = $req->fetchAll();
         return $commentaires;
     }
     
-    public function get_commentaires_by_id_billet($offset, $limit, $id)
+    // récupération des commentaires en fonction de l'id du billet correspondant
+    public function getCommentairesByIdBillet($offset, $limit, $id)
     {
         $offset = (int)$offset;
         $limit = (int)$limit;
         $id = (int)$id;
         
-        $sql = 'SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y\') AS date_comm, DATE_FORMAT(date_commentaire, \'%Hh%i\') AS heure_comm FROM commentaires WHERE id_billet=? ORDER BY date_commentaire LIMIT ?, ?';
+        $sql = 'SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y\') AS dateComm, DATE_FORMAT(date_commentaire, \'%Hh%i\') AS heureComm, valide FROM commentaires WHERE id_billet=:id ORDER BY date_commentaire LIMIT :offset, :limit';
         $req = $this->executerRequete($sql, array($id, $offset, $limit));
         
         $commentairesById = $req->fetchAll();
         return $commentairesById;
     }
     
-    // Ajout des nouveaux commentaires à la BDD
-    public function post_commentaire(Commentaire $commentaire)
+    // Ajout des nouveaux commentaires à la BDD, validés par défaut
+    public function postCommentaire(Commentaire $commentaire)
     {
         if (isset($_POST['id_billet']))
         {
             $sql = 'INSERT INTO commentaires (id_billet, auteur, commentaire, date_commentaire) VALUES(?, ?, ?, NOW())';
-            $array = array($commetaire->id_billet, $commentaire->auteur, $commentaire->commentaire);
-            $this->executerRequete($sql, $array);
+            $array = array($commentaire->id_billet(), $commentaire->auteur(), $commentaire->commentaire());
+            $donnees = $this->executerRequete($sql, $array);
+            
+            return $donnees;
         }
     }
     
     // Supprimer un commentaire de la BDD
-    public function delete_commentaire(Commentaire $commentaire)
+    public function deleteCommentaire($id)
     {
         $sql = 'DELETE FROM commentaires WHERE id = ?';
-        $this->executerRequete($sql, array($commentaire->id));
+        $this->executerRequete($sql, array($id));
     }
     
-    // pour retourner le nombre total de commentaires 
-    public function count_commentaires()
+    // Mise à jour des commentaires pour les afficher après validation
+    public function updateCommentaire($valide, $id)
     {
-        $sql = 'SELECT COUNT(*) AS nb_commentaires FROM commentaires';
+        $sql = 'UPDATE commentaires SET valide=? WHERE id = ?';
+        $array = array($valide, $id);
+        $nouveauCom = $this->executerRequete($sql, $array);
+        
+        return $nouveauCom;
+    }
+    
+    // pour retourner le nombre total de commentaires signalés à valider
+    public function countCommentairesAValider()
+    {
+        $sql = 'SELECT COUNT(*) AS nbCommentaires FROM commentaires WHERE valide = \'FALSE\'';
         $req = $this->executerRequete($sql);
         $resultat = $req->fetch();
-        $nb_commentaires = (int)$resultat['nb_commentaires'];
+        $nbCommentaires = (int)$resultat['nbCommentaires'];
         
-        return $nb_commentaires;
+        return $nbCommentaires;
+    }
+    
+    // pour retourner le nombre total de commentaires validés et postés
+    public function countCommentairesValide()
+    {
+        $sql = 'SELECT COUNT(*) AS nbCommentaires FROM commentaires WHERE valide = \'TRUE\'';
+        $req = $this->executerRequete($sql);
+        $resultat = $req->fetch();
+        $nbCommentaires = (int)$resultat['nbCommentaires'];
+        
+        return $nbCommentaires;
     }
 }
