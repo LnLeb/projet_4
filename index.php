87d3fe531c319a91cf8e7f639ef3e0e0612e15ab
LnLeb <helene.d.m@wanadoo.fr>
2017-11-15 14:22:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-// On inclut le routeur qui gère l'affichage des pages en fonction des paramètres transmis par l'url ou affiche des messages d'erreur
+
+// création et utilisation d'un objet routeur
 require('controleur/Routeur.php');
 
-// création d'un objet routeur pour utiliser sa méthode requête
 $routeur = new Routeur();
 $routeur->routerRequete();
