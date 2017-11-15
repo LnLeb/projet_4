b5b1e029dccc6be907bdb4e683f526c32c20df6b
LnLeb <helene.d.m@wanadoo.fr>
2017-11-15 14:28:43 +0100

    Update ControleurAdmin.php
    Mise à jour de toutes les méthodes nécessaires pour l'administration

--- controleur/ControleurAdmin.php
+++ controleur/ControleurAdmin.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once ('modele/BilletManager.php');
-require_once ('modele/CommentaireManager.php');
-require_once ('vue/Vue.php');
+require_once('modele/BilletManager.php');
+require_once('modele/CommentaireManager.php');
+require_once('vue/Vue.php');
+require_once('modele/Billet.php');
 
 class ControleurAdmin
 {
@@ -15,35 +16,101 @@ class ControleurAdmin
         $this->commentaire = new CommentaireManager;
     }
     
+    // page d'accueil de l'adminitration
     public function admin()
     {
-        if ((isset($_POST['identifiant']) AND $_POST['identifiant'] == "Jean.Forteroche" AND isset($_POST['motdepasse']) AND $_POST['motdepasse'] == "alaska2.0") OR (isset($_SESSION['identifiant'])))
-        {
-            $_SESSION['identifiant']=$_POST['identifiant'];
-            
-            $billets = $this->billet->get_billets(0, 500);
+        $billets = $this->billet->getBillets(0, $this->billet->countBillets());
+        $commentaires = $this->commentaire->getCommentairesAValider(0, $this->commentaire->countCommentairesAValider());
         
-            // sécurisation de l'affichage
-            foreach($billets as $cle=>$billet)
-            {
-                $billets[$cle]['titre'] = htmlspecialchars($billet['titre']);
-            }
-            
-            // on affiche la page (vue)
-            $vue = new Vue('admin');
-            $vue->generer(array('billets' => $billets));
-        }
-        else 
+        // sécurisation de l'affichage
+        foreach($billets as $cle=>$billet)
         {
-            require('index.php');
+            $billets[$cle]['titre'] = htmlspecialchars($billet['titre']);
         }
+        foreach($commentaires as $cle=>$commentaire)
+        {
+            $commentaires[$cle]['auteur'] = htmlspecialchars($commentaire['auteur']);
+            $commentaires[$cle]['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire']));
+        }
+        
+        // on génère la vue
+        $vue = new Vue('admin');
+        $vue->generer(array('billets' => $billets, 'commentaires' => $commentaires));
     }
     
-    public function deconnexion()
+    // page de création d'un nouveau chapitre
+    public function nouveauChapitre()
     {
-        if(isset($_SESSION['identifiant'])) {
-        session_destroy(); 
-        } 
-        require('index.php');
+        $vue = new Vue('nouveauChapitre');
+        $vue->generer(array());
+    }
+    
+    // retour à l'acceuil si erreur de mot de passe ou d'identifiant
+    public function errConnexion()
+    {
+        header('Location: index.php?#connexion');
+    }
+    
+    // pour poster un nouveau chapitre dans la BDD
+    public function postBillet($titre, $extrait, $contenu)
+    {
+        $donnees = array('titre' => $titre,
+                        'extrait' => $extrait,
+                        'contenu' => $contenu);
+        
+        $billet = new Billet($donnees);
+        $billet->setTitre($titre);
+        $billet->setExtrait($extrait);
+        $billet->setContenu($contenu);
+        
+        $this->billet->postBillet($billet);
+        header('Location: index.php?action=admin#nouveauChapitre');
+    }
+    
+    // pour afficher la page de mise à jour du billet sélectionné
+    public function update($id)
+    {
+        $billet = $this->billet->getBilletById($id);
+        
+        $vue = new Vue('miseAJour');
+        $vue->generer(array('billet' => $billet));
+    }
+    
+    // pour mettre à jour un chapitre déjà posté dans la BDD
+    public function updateBillet($titre, $extrait, $contenu, $id)
+    {
+        $this->billet->updateBillet($titre, $extrait, $contenu, $id);
+        header('Location: index.php?action=admin');
+    }
+    
+    // suppression d'un billet
+    public function deleteBillet($id)
+    {
+        $this->billet->deleteBillet($id);
+        header('Location: index.php?action=admin');
+    }
+    
+    // validation d'un commentaire signalé
+    public function updateCom($id)
+    {
+        $this->commentaire->updateCommentaire('TRUE', $id);
+        header('Location: index.php?action=admin#gestionComm');
+    }
+    
+    // suppression d'un commentaire signalé
+    public function deleteCom($id)
+    {
+        $this->commentaire->deleteCommentaire($id);
+        header('Location: index.php?action=admin#gestionComm');
+    }
+    
+    // déconnexion de l'espace admin
+    public function deconnexion() 
+    {
+        if(isset($_SESSION['identifiant'])) 
+        {
+            session_destroy();
+        }
+        header('Location: index.php');
     }
 }
