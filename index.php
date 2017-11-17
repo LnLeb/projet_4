c1bfb7b514d1825ae8d85bc7bd7f728934af0798
LnLeb <helene.d.m@wanadoo.fr>
2017-11-17 12:25:44 +0100

    Update index.php
    ajout des sessions

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-
+session_start(); 
+if(!isset($_SESSION['info'])) 
+{
+    $_SESSION['info'] = '';    
+}
 // création et utilisation d'un objet routeur
 require('controleur/Routeur.php');
 
