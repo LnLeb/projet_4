abaf7c7b3d3c0b912105825bab7883dfd005d3e8
LnLeb <helene.d.m@wanadoo.fr>
2017-11-17 14:03:02 +0100

    Update nouveauChapitre.php

--- vue/nouveauChapitre.php
+++ vue/nouveauChapitre.php
@@ -4,24 +4,28 @@
     <ul>
         <li><a href='index.php?action=admin'>Chapitres en ligne</a></li>
         <li><a href='index.php?action=admin&rubrique=nouveauChapitre'>Nouveau chapitre</a></li>
-        <li><a href='#gestionComm'>Gestion des commentaires</a></li>
+        <li><a href='index.php?action=admin#gestionComm'>Gestion des commentaires</a></li>
         <li><a href='index.php?action=admin&rubrique=deconnexion'>Déconnexion</a></li>
     </ul>
 </nav>
-
-<!-- zone de création d'un nouveau chapitre avec tinyMce -->
-<article class="articleChapitre">
-    <h2>Création d'un nouveau billet : </h2>
-    <form method="post" action="index.php?action=admin&rubrique=postBillet">
-        <label for="titre">Titre : </label><br>
-        <input type="text" name="titre" id="titre"><br>
-        <label for="extrait">Extrait : </label><br>
-        <textarea name="extrait" id="extrait" rows="8" ></textarea><br>
-        <label for="contenu">Contenu : </label><br>
-        <textarea name="contenu" id="contenuAdmin" rows="40"></textarea><br>
-        <input type="submit" value="publier">
-    </form>
-</article>
+<section id="adminChap">
+    <div class="administration">
+        <!-- zone de création d'un nouveau chapitre avec tinyMce -->
+        <article class="articleChapitre">
+            <h2>Création d'un nouveau billet : </h2>
+            <form method="post" action="index.php?action=admin&rubrique=postBillet">
+                <label for="titre">Titre : </label><br>
+                <input type="text" name="titre" id="titre"><br>
+                <label for="extrait">Extrait : </label><br>
+                <textarea name="extrait" id="extrait" rows="8" ></textarea><br>
+                <label for="contenu">Contenu : </label><br>
+                <textarea name="contenu" id="contenu" rows="40"></textarea><br>
+                <input type="hidden" name="id" value="<?= $nouvelId ?>">
+                <input type="submit" value="publier" class="bouton">
+            </form>
+        </article>
+    </div>
+</section>
 
 <script type="text/javascript" src="contenu/tinymce/tinymce.min.js"></script>
 <script type="text/javascript">
