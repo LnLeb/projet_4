c67e2c2c9ec4eb0776f8679bc17198cb58567113
LnLeb <helene.d.m@wanadoo.fr>
2017-11-17 14:02:27 +0100

    Update miseAJour.php

--- vue/miseAJour.php
+++ vue/miseAJour.php
@@ -5,25 +5,29 @@
     <ul>
         <li><a href='index.php?action=admin'>Chapitres en ligne</a></li>
         <li><a href='index.php?action=admin&rubrique=nouveauChapitre'>Nouveau chapitre</a></li>
-        <li><a href='#gestionComm'>Gestion des commentaires</a></li>
+        <li><a href='index.php?action=admin#gestionComm'>Gestion des commentaires</a></li>
         <li><a href='index.php?action=admin&rubrique=deconnexion'>Déconnexion</a></li>
     </ul>
 </nav>
 
-<!-- Zone de mise à jour du billet sélectionné, avec TyniMce -->
-<article class="articleChapitre">
-    <h2>Mise à jour d'un chapitre : </h2>
-    <form method="post" action="index.php?action=admin&rubrique=update&id=<?= $billet['id']; ?>">
-        <label for="titre">Titre : </label><br>
-        <input type="text" name="titre" id="titre" value="<?= $billet['titre']; ?>"><br>
-        <label for="extrait">Extrait : </label><br>
-        <textarea name="extrait" id="extrait" rows="8" ><?= $billet['extrait']; ?></textarea><br>
-        <label for="contenu">Contenu : </label><br>
-        <textarea name="contenu" id="contenuAdmin" rows="40"><?= $billet['contenu']; ?></textarea><br>
-        <input type="submit" value="mettre à jour">
-    </form>
-</article>
-
+<section id="adminUpdate">
+    <div class="administration">
+        <!-- Zone de mise à jour du billet sélectionné, avec TyniMce -->
+        <article class="articleChapitre">
+            <h2>Mise à jour d'un chapitre : </h2>
+            <form method="post" action="index.php?action=admin&rubrique=updateBillet&id=<?= $billet['id']; ?>">
+                <label for="titre">Titre : </label><br>
+                <input type="text" name="titre" id="titre" value="<?= $billet['titre']; ?>"><br>
+                <label for="extrait">Extrait : </label><br>
+                <textarea name="extrait" id="extrait" rows="8" ><?= $billet['extrait']; ?></textarea><br>
+                <label for="contenu">Contenu : </label><br>
+                <textarea name="contenu" id="contenuAdmin" rows="40"><?= $billet['contenu']; ?></textarea><br>
+                <input type="submit" value="mettre à jour" class="bouton">
+            </form>
+        </article>
+    </div>
+</section>
+        
 <script type="text/javascript" src="contenu/tinymce/tinymce.min.js"></script>
 <script type="text/javascript">
     tinyMCE.init({
