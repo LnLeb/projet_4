c112fe45ec55ce9dbf7b4f10e3e6c94ac8e8f1bd
LnLeb <helene.d.m@wanadoo.fr>
2017-11-17 13:56:23 +0100

    Update ControleurAdmin.php

--- controleur/ControleurAdmin.php
+++ controleur/ControleurAdmin.php
@@ -41,30 +41,36 @@ class ControleurAdmin
     // page de création d'un nouveau chapitre
     public function nouveauChapitre()
     {
+        $nouvelId = (($this->billet->countBillets())+1);
+        
         $vue = new Vue('nouveauChapitre');
-        $vue->generer(array());
+        $vue->generer(array('nouvelId' => $nouvelId));
     }
     
     // retour à l'acceuil si erreur de mot de passe ou d'identifiant
     public function errConnexion()
     {
+        $_SESSION['info'] = 'Erreur identifiant ou mot de passe';
         header('Location: index.php?#connexion');
     }
     
     // pour poster un nouveau chapitre dans la BDD
-    public function postBillet($titre, $extrait, $contenu)
+    public function postBillet($id, $titre, $extrait, $contenu)
     {
-        $donnees = array('titre' => $titre,
+        $donnees = array('id' => $id,
+                        'titre' => $titre,
                         'extrait' => $extrait,
                         'contenu' => $contenu);
         
         $billet = new Billet($donnees);
+        $billet->setId($id);
         $billet->setTitre($titre);
         $billet->setExtrait($extrait);
         $billet->setContenu($contenu);
         
         $this->billet->postBillet($billet);
-        header('Location: index.php?action=admin#nouveauChapitre');
+        $_SESSION['info'] = 'Votre nouveau chapitre a bien été publié!';
+        header('Location: index.php?action=admin#chapitresEnLigne');
     }
     
     // pour afficher la page de mise à jour du billet sélectionné
@@ -80,13 +86,15 @@ class ControleurAdmin
     public function updateBillet($titre, $extrait, $contenu, $id)
     {
         $this->billet->updateBillet($titre, $extrait, $contenu, $id);
-        header('Location: index.php?action=admin');
+        $_SESSION['info'] = 'Votre chapitre a bien été mis à jour!';
+        header('Location: index.php?action=admin#chapitresEnLigne');
     }
     
     // suppression d'un billet
     public function deleteBillet($id)
     {
         $this->billet->deleteBillet($id);
+        $_SESSION['info'] = 'Le chapitre a été supprimé.';
         header('Location: index.php?action=admin');
     }
     
@@ -94,6 +102,7 @@ class ControleurAdmin
     public function updateCom($id)
     {
         $this->commentaire->updateCommentaire('TRUE', $id);
+        $_SESSION['info'] = 'Le commentaire est validé.';
         header('Location: index.php?action=admin#gestionComm');
     }
     
@@ -101,16 +110,14 @@ class ControleurAdmin
     public function deleteCom($id)
     {
         $this->commentaire->deleteCommentaire($id);
+        $_SESSION['info'] = 'Le commentaire est supprimé.';
         header('Location: index.php?action=admin#gestionComm');
     }
     
     // déconnexion de l'espace admin
     public function deconnexion() 
     {
-        if(isset($_SESSION['identifiant'])) 
-        {
-            session_destroy();
-        }
+        session_destroy();
         header('Location: index.php');
     }
 }
