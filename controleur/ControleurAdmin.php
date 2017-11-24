d9c1dcede033f8de4aba19f92f742e43729c351d
LnLeb <helene.d.m@wanadoo.fr>
2017-11-24 17:20:02 +0100

    Update ControleurAdmin.php

--- controleur/ControleurAdmin.php
+++ controleur/ControleurAdmin.php
@@ -2,10 +2,8 @@
 
 require_once('modele/BilletManager.php');
 require_once('modele/CommentaireManager.php');
-require_once('modele/ChapitreManager.php');
 require_once('vue/Vue.php');
 require_once('modele/Billet.php');
-require_once('modele/Chapitre.php');
 
 class ControleurAdmin
 {
@@ -17,28 +15,29 @@ class ControleurAdmin
     {
         $this->billet = new BilletManager;
         $this->commentaire = new CommentaireManager;
-        $this->chapitre = new ChapitreManager;
     }
     
     // page d'accueil de l'adminitration
     public function admin()
     {
-        $billets = $this->billet->getBillets(0, $this->billet->countBillets());
-        $chapitres = $this->chapitre->getChapitres(0, $this->chapitre->countChapitres());
+        $billetsPublics = $this->billet->getBilletsPublics(0, $this->billet->countBillets());
+        $billetsPrives = $this->billet->getBilletsPrives(0, $this->billet->countBilletsPrives());
         $commentaires = $this->commentaire->getCommentairesAValider(0, $this->commentaire->countCommentairesAValider());
 
         // on génère la vue
         $vue = new Vue('admin');
-        $vue->generer(array('billets' => $billets,
-                            'chapitres' => $chapitres,
+        $vue->generer(array('billetsPublics' => $billetsPublics,
+                            'billetsPrives' => $billetsPrives,
                             'commentaires' => $commentaires));
     }
     
     // page de création d'un nouveau chapitre
-    public function nouveauChapitre()
+    public function nouveauBillet()
     {
+        $nouvelId = ($this->billet->countBillets() + $this->billet->countBilletsPrives() + 1);
+        
         $vue = new Vue('nouveauChapitre');
-        $vue->generer(array());
+        $vue->generer(array('nouvelId' => $nouvelId));
     }
     
     // retour à l'acceuil si erreur de mot de passe ou d'identifiant
@@ -51,77 +50,26 @@ class ControleurAdmin
     // affiche un chapitre en cours d'écriture pour avoir un apperçu
     public function apercu($id)
     {
-        $chapitre = $this->chapitre->getChapitreById($id);
+        $billet = $this->billet->getBilletPriveById($id);
         
         $vue = new Vue('apercu');
-        $vue->generer(array('chapitre' => $chapitre));
+        $vue->generer(array('billet' => $billet));
     }
     
-    // affiche la page de mise à jour du chapitre en cours d'écriture
-    public function updateChap($id)
+    public function postBillet($titre, $extrait, $contenu, $id)
     {
-        $chapitre = $this->chapitre->getChapitreById($id);
-        
-        $vue = new Vue('miseAJourChapitre');
-        $vue->generer(array('chapitre' => $chapitre));
-    }
-    
-    // mise à jour du chapitre en cours d'écriture
-    public function updateChapitre($titre, $extrait, $contenu, $id)
-    {
-        $this->chapitre->updateChapitre($titre, $extrait, $contenu, $id);
-        $_SESSION['info'] = 'Votre chapitre a bien été mis à jour!';
-        header('Location: index.php?action=admin');
-    }
-    
-    // suppression du chapitre en cours d'écriture
-    public function deleteChapitre($id)
-    {
-        $this->chapitre->deleteChapitre($id);
-        $_SESSION['info'] = 'Le chapitre a été supprimé.';
-        header('Location: index.php?action=admin');
-    }
-    
-    // genere la vue du chapitre à publier uniquement pour récupérer les informations et les changer de table
-    public function publierChap($id)
-    {
-        $chapitre = $this->chapitre->getChapitreById($id);
-        $vue = new Vue('postBillet');
-        $vue->generer(array('chapitre' => $chapitre));
-    }
-    
-    // pour poster un nouveau chapitre dans la BDD
-    public function postBillet($id, $titre, $extrait, $contenu)
-    {
-        $nouvelId = ($this->billet->countBillets() +1);
-        $donnees = array('id' => $nouvelId,
-                        'titre' => $titre,
+        $donnees = array('titre' => $titre, 
                         'extrait' => $extrait,
-                        'contenu' => $contenu);
+                        'contenu' => $contenu, 
+                        'id' => $id);
         
         $billet = new Billet($donnees);
-        $billet->setId($nouvelId);
+        $billet->setId($id);
         $billet->setTitre($titre);
         $billet->setExtrait($extrait);
         $billet->setContenu($contenu);
         
         $this->billet->postBillet($billet);
-        $_SESSION['info'] = 'Votre nouveau chapitre a bien été publié!';
-        header('Location: index.php?action=admin#chapitresEnLigne');
-    }
-    
-    public function postChapitre($titre, $extrait, $contenu)
-    {
-        $donnees = array('titre' => $titre, 
-                        'extrait' => $extrait,
-                        'contenu' => $contenu);
-        
-        $chapitre = new Chapitre($donnees);
-        $chapitre->setTitre($titre);
-        $chapitre->setExtrait($extrait);
-        $chapitre->setContenu($contenu);
-        
-        $this->chapitre->postChapitre($chapitre);
         $_SESSION['info'] = 'Votre chapitre en cours d\'écriture est enregistré';
         header('Location: index.php?action=admin');
     }
@@ -141,6 +89,14 @@ class ControleurAdmin
         $this->billet->updateBillet($titre, $extrait, $contenu, $id);
         $_SESSION['info'] = 'Votre chapitre a bien été mis à jour!';
         header('Location: index.php?action=admin#chapitresEnLigne');
+    }
+    
+    // publication d'un billet
+    public function publier($id)
+    {
+        $this->billet->rendrePublic($id);
+        $_SESSION['info'] = 'Le chapitre est publié et maintenant visible depuis l\'accueil';
+        header('Location: index.php?action=admin');
     }
     
     // suppression d'un billet
